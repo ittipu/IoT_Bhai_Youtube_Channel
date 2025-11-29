dc36f910a2dc0c81238988aca583df5344cc5e1b
Md. Kamruzzaman Tipu <36630270+ittipu@users.noreply.github.com>
2025-11-29 15:16:47 +0600

    Update sending_and_receving_SMS_using_lilygo_sim800l.ino

--- LilyGo SIM800L 2G Dev Board/2. Send and Receive SMS using LilyGo SIM800L/sending_and_receving_SMS_using_lilygo_sim800l/sending_and_receving_SMS_using_lilygo_sim800l.ino
+++ LilyGo SIM800L 2G Dev Board/2. Send and Receive SMS using LilyGo SIM800L/sending_and_receving_SMS_using_lilygo_sim800l/sending_and_receving_SMS_using_lilygo_sim800l.ino
@@ -5,7 +5,7 @@
 #define GSM_PIN ""
 #include <TinyGsmClient.h>
 
-#define ADMIN_NUMBER "+8801715497977"
+#define ADMIN_NUMBER "Put Your Phone Number"
 
 #ifdef DUMP_AT_COMMANDS
 #include <StreamDebugger.h>
